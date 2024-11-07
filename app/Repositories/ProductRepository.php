f6734745a8d0748d248430cf699575c0d18c73f2
h-iranshahi <h.iranshahi67@gmail.com>
2024-11-07 13:58:38 +0330

    Improve ProductRepository

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -32,4 +32,27 @@ class ProductRepository
         $product = $this->find($id);
         return $product ? $product->delete() : false;
     }
+
+    public function getProducts($request)
+    {
+        $perPage = $request->input('per_page', 10);  
+
+        $query = Product::query();
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        if ($request->has('min_price')) {
+            $query->where('price', '>=', $request->input('min_price'));
+        }
+
+        if ($request->has('max_price')) {
+            $query->where('price', '<=', $request->input('max_price'));
+        }
+
+        return $query->paginate($perPage);
+    }
+
+
 }
